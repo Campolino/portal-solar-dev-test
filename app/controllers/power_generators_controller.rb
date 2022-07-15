61b171f52b70ac54adee75450ae36af0702e8a69
Lucas Eduardo Campolino <campolino.l@gmail.com>
2022-07-14 21:34:33 -0300

    Busca retorna pelo nome ou descrição do produto

--- app/controllers/power_generators_controller.rb
+++ app/controllers/power_generators_controller.rb
@@ -5,7 +5,13 @@ class PowerGeneratorsController < ApplicationController
 
   def search
     @parameters = params[:q].downcase
-    @power_generators = PowerGenerator.all.where('name ILIKE :search', search: "%#{@parameters}%")
-    render 'index'
+    @power_generators = PowerGenerator.all.where('name ILIKE :search', search: "%#{@parameters}%").or(PowerGenerator.all.where('description ILIKE :search', search: "%#{@parameters}%"))
+    if @power_generators.any?
+      render :index
+    else
+      @power_generators = PowerGenerator.all
+      flash.now[:alert] = 'Nenhum produto encontrado para essa busca'
+      render :index
+    end
   end  
 end
--- spec/system/user_search_generators_spec.rb
+++ spec/system/user_search_generators_spec.rb
@@ -7,10 +7,22 @@ context 'Usuário busca por geradores' do
       within '.simple-form' do
         fill_in 'q',	with: 'trifásico' 
         click_on 'Pesquisar'
-
       end
 
-      expect('page').not_to have_content 'MONOFÁSICO'
+      expect(page).to have_content 'TRIFÁSICO 380V'
+      expect(page).not_to have_content 'YC600 MONOFÁSICO'
+      expect(page).not_to have_content 'Nenhum produto encontrado para essa busca'
+    end
+
+    it 'e busca não bate com nenhum gerador' do
+      visit '/'
+      within '.simple-form' do
+        fill_in 'q',	with: 'casa' 
+        click_on 'Pesquisar'
+      end
+
+      expect(page).to have_content 'Nenhum produto encontrado para essa busca'
+      expect(page).to have_content 'YC600 MONOFÁSICO'
     end
   end
 end
